4a2f0f649e61289d5e148267597cd45699ee59e0
Aashish Karki <aashish.y2z@gmail.com>
2019-07-17 15:12:11 +0545

    update images to import for svg and jpg support

---
+++ python/imagesToImport/index.js
@@ -0,0 +1,15 @@
+import datepicker from './datepicker.png';
+import dropdown_un from './dropdown_un.png';
+import addpatient from './addpatient.svg';
+import dropdown from './dropdown.png';
+import delete from './delete.png';
+import close from './close.png';
+
+export {
+  datepicker,
+  dropdown_un,
+  addpatient,
+  dropdown,
+  delete,
+  close,
+};
